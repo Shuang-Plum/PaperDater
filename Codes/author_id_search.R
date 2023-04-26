7ba0dde9b5af1b4a2d1d8a498e60847005505a7a
Shuang <57366173+Shuang-Plum@users.noreply.github.com>
2023-04-26 10:03:03 -0400

    Add files via upload

---
+++ Codes/author_id_search.R
@@ -0,0 +1,22 @@
+# return publication Mesh terms for year in range or the most recent 5000
+author_id_search<-function(authorid, starty, endy) {
+  
+  authorid<-paste0(authorid,' [auid]')
+  # set time to be publication date
+  ydat<-paste0('(',starty,':',endy,' [dp])')
+  myquery<-paste0('(',authorid,') AND ',ydat)
+  
+  ris.res <- EUtilsSummary(myquery,type="esearch", db="pubmed", retmax=5000)
+  
+  # # save query summary to a txt file for reference
+  # sink('Data/journal_query_summary.txt')
+  # summary(ris.res)
+  # sink()
+  # # add newline by the end of text to avoid warning when open by read.delim
+  # write("\n", file = "Data/journal_query_summary.txt",append = TRUE, sep = "\n")
+  # 
+
+  
+  return(ris.res)
+  
+}
